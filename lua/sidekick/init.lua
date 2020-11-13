ffd09ae7a0890d0bc501e134e839b2019ab017ef
El Piloto <Hidden>
2020-11-13 22:44:09 +0000

    Add jump_to_definition and .luacheckrc file

--- lua/sidekick/init.lua
+++ lua/sidekick/init.lua
@@ -1,18 +1,52 @@
---TODO(elpiloto): Using functions inside vim lua module e.g. for splitting strings or counting elements.
-ts_query = require 'vim.treesitter.query'
-nts_locals = require('nvim-treesitter.locals')
-nts_parsers = require('nvim-treesitter.parsers')
-nts_query = require('nvim-treesitter.query')
-ts_utils = require('nvim-treesitter.ts_utils')
-sk_outline = require('sidekick.outline')
 local api = vim.api
 
-local loaded_buffers = {}
-local open_windows = {}
+local sk_outline = require('sidekick.outline')
+
+local M = {}
+
+-- This holds a mapping from a dict[buffer_id --> dict[line_nr --> cursor_position]]
+M.per_buffer_jump_info = {}
+M.open_windows = {}
+M.last_parsed_buf = -1
+
+--TODO(elpiloto): Jump from outline to definition.
+--  1. Associate buffer information (e.g. line number) with sidekick entries.
+--  2. Make keymappings for Outline window.
+
+-- TODO(elpiloto): After jumping to definition, scroll screen upwards.  Make this configurable.
+
+-- TODO(elpiloto): Set position when entering sidekick window. Either based on:
+-- 1. Last position (even if we closed).
+-- 2. Position in buffer.
+
+--TODO(elpiloto): Decouple rendering from outline in order to:
+-- 1. Allow smarter rendering (e.g. isolated top-level nodes should be displayed as "outerNode"
+-- 2. interface to allow other tag definition backends (e.g. LSP or ctags).
+
+-- TODO(elpiloto): Add custom fold highlighting while we wait for neovim bug
+-- fix.
+
+--TODO(elpiloto): Better config: documentation and error-checking.
+
+--TODO(elpiloto): Show LSP signatureHelp.
+
+--TODO(elpiloto): Option to display filename in sidekick.
+
+--TODO(elpiloto): Display empty outline window when appropriate.
+-- Currently we simply don't open an outline window if the current buffer is empty or the filetype doesn't work.
+
+--TODO(elpiloto): Update outline on editor events:
+-- 1. buffer saved
+-- 2. change active window
+
+-- TODO(elpiloto): Document highlight groups so that colorschemes can
+-- explicitly support them.
+
+-- TODO(elpiloto): Disable context.vim somehow. Look it up. Read some docs dammit.
 
 -- Splits string at new lines into table.
 local function split_str(str)
-	lines = {}
+	local lines = {}
 	for s in str:gmatch("[^\r\n]+") do
 		table.insert(lines, s)
 	end
@@ -20,23 +54,24 @@ local function split_str(str)
 end
 
 local function getSidekickText()
-header = [[
-   
+	-- luacheck: push ignore 613 (trailing whitespaces)
+	M.header = [[
+
    _____ _     __     __ __ _      __   
   / ___/(_)___/ /__  / //_/(_)____/ /__ 
   \__ \/ / __  / _ \/ ,<  / / ___/ //_/ 
  ___/ / / /_/ /  __/ /| |/ / /__/ ,<    
 /____/_/\__,_/\___/_/ |_/_/\___/_/|_|   
 =======================================
- 
+
 ]]
-lines = {}
-num_lines = 0
-for s in header:gmatch("[^\r\n]+") do
-    table.insert(lines, s)
-	num_lines = num_lines + 1
-end
-header_and_dude = [[
+	local lines = {}
+	local num_lines = 0
+	for s in M.header:gmatch("[^\r\n]+") do
+		table.insert(lines, s)
+		num_lines = num_lines + 1
+	end
+	local header_and_dude = [[
          ..      
         :  :     _____ _     __     __ __ _      __   
          ``     / ___/(_)___/ /__  / //_/(_)____/ /__ 
@@ -44,40 +79,41 @@ header_and_dude = [[
         _| >   ___/ / / /_/ /  __/ /| |/ / /__/ ,<    
 o________/    /____/_/\__,_/\___/_/ |_/_/\___/_/|_|   
     o___/     =======================================
- 
+
 ]]
-header_and_poof = [[
-                
+	local header_and_poof = [[
+
    .( * .        _____ _     __     __ __ _      __   
  .*  .  ) .     / ___/(_)___/ /__  / //_/(_)____/ /__ 
 . . POOF .* .   \__ \/ / __  / _ \/ ,<  / / ___/ //_/ 
  '* . (  .) '  ___/ / / /_/ /  __/ /| |/ / /__/ ,<    
   ` ( . *     /____/_/\__,_/\___/_/ |_/_/\___/_/|_|   
               =======================================
-	 
+
 ]]
-return split_str(header), split_str(header_and_dude), split_str(header_and_poof)
+	-- luacheck: pop
+	return split_str(M.header), split_str(header_and_dude), split_str(header_and_poof)
 end
 
-local function animateBufferOpen(win, header, matches, poof)
+local function animate_buffer_open(win, buf, header, matches, poof)
 	-- Smoothly expand window width, contract a bit, and then expand to final size.
-	final_width=40
-	widths = {60, 50, 52}
-	speeds = {100, 200, 200}
-	prev_w = 1
+	local final_width=40
+	local widths = {60, 50, 52}
+	local speeds = {100, 200, 200}
+	local prev_w = 1
+	local speed = nil
 	for i, target_w in pairs(widths) do
 		speed = speeds[i]
+		local step_dir = -1
 		if target_w > prev_w then
 			step_dir = 1
-		else
-			step_dir = -1
 		end
 		-- Gradually modify our width to target width
-		for i = prev_w, target_w, step_dir do
-			new_width_fn = function()
-				vim.api.nvim_win_set_width(win, i)
+		for j = prev_w, target_w, step_dir do
+			local new_width_fn = function()
+				api.nvim_win_set_width(win, j)
 				-- Need this for smooth animation.
-				vim.api.nvim_command('redraw')
+				api.nvim_command('redraw')
 			end
 			vim.defer_fn(new_width_fn, speed)
 		end
@@ -85,60 +121,66 @@ local function animateBufferOpen(win, header, matches, poof)
 	end
 	-- finally after all that has been done, let's set the final text
 	if poof then
-		final_text_fn = function()
+		local final_text_fn = function()
 			-- Display matches
-			vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-			vim.api.nvim_buf_set_lines(buf, 0, #poof, false, poof)
+			api.nvim_buf_set_option(buf, 'modifiable', true)
+			api.nvim_buf_set_lines(buf, 0, #poof, false, poof)
 			-- Apparently this needs to be set after we insert text.
-			vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+			api.nvim_buf_set_option(buf, 'modifiable', false)
 		end
 		vim.defer_fn(final_text_fn, speed*6)
 	end
 	-- finally after all that has been done, let's set the final text
-	final_text_fn = function()
+	local final_text_fn = function()
 		-- Display matches
-		vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-		vim.api.nvim_buf_set_lines(buf, 0, #header, false, header)
-		vim.api.nvim_buf_set_lines(buf, #header, #header+#matches, false, matches)
+		api.nvim_buf_set_option(buf, 'modifiable', true)
+		api.nvim_buf_set_lines(buf, 0, #header, false, header)
+		api.nvim_buf_set_lines(buf, #header, #header+#matches, false, matches)
 		-- Apparently this needs to be set after we insert text.
-		vim.api.nvim_buf_set_option(buf, 'modifiable', false)
-		vim.api.nvim_win_set_width(win, final_width)
+		api.nvim_buf_set_option(buf, 'modifiable', false)
+		api.nvim_win_set_width(win, final_width)
 		-- Need this for smooth animation.
-		vim.api.nvim_command('redraw')
+		api.nvim_command('redraw')
 	end
 	vim.defer_fn(final_text_fn, speed*12)
 end
 
-local function bufferOpen(win, header, matches)
+local function bufferOpen(win, buf, header, matches)
 	-- Smoothly expand window width, contract a bit, and then expand to final size.
-	final_width=40
-	vim.api.nvim_win_set_width(win, final_width)
-	vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-	vim.api.nvim_buf_set_lines(buf, 0, #header, false, header)
-	vim.api.nvim_buf_set_lines(buf, #header, #header+#matches, false, matches)
+	local final_width=40
+	api.nvim_win_set_width(win, final_width)
+	api.nvim_buf_set_option(buf, 'modifiable', true)
+	api.nvim_buf_set_lines(buf, 0, #header, false, header)
+	api.nvim_buf_set_lines(buf, #header, #header+#matches, false, matches)
 	-- Apparently this needs to be set after we insert text.
-	vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+	api.nvim_buf_set_option(buf, 'modifiable', false)
 
 end
 
+-- NB: Must be called from within sidekick window.
+local function add_keymappings()
+	-- jump to definition on <CR>
+	api.nvim_buf_set_keymap(0, 'n', '<CR>', ':lua require "sidekick".jump_to_definition()<CR>', { silent = true })
+end
+
 local function make_outline_window(win_name)
-	--local win_name = 'Sidekick' .. tostring(vim.api.nvim_get_current_buf())
-	vim.api.nvim_command('keepalt botright vertical 1 split ' .. win_name)
-	local win = vim.api.nvim_get_current_win()
-	local buf = vim.api.nvim_get_current_buf()
+	--local win_name = 'Sidekick' .. tostring(api.nvim_get_current_buf())
+	api.nvim_command('keepalt botright vertical 1 split ' .. win_name)
+	local win = api.nvim_get_current_win()
+	local buf = api.nvim_get_current_buf()
 
 	--Disable wrapping
-	vim.api.nvim_win_set_option(win, 'wrap', false)
-	vim.api.nvim_win_set_option(win, 'list', false)
-	vim.api.nvim_win_set_option(win, 'number', false)
-	vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
-	vim.api.nvim_buf_set_option(buf, 'swapfile', false)
-	vim.api.nvim_win_set_option(win, 'wrap', false)
+	api.nvim_win_set_option(win, 'wrap', false)
+	api.nvim_win_set_option(win, 'list', false)
+	api.nvim_win_set_option(win, 'number', false)
+	api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+	api.nvim_buf_set_option(buf, 'swapfile', false)
+	api.nvim_win_set_option(win, 'wrap', false)
 	vim.bo.buflisted = false
 	vim.bo.modifiable = false
 	vim.bo.textwidth = 0
 	vim.bo.filetype = 'sidekick'
-	vim.api.nvim_command('hi NonText guifg=bg')
+	api.nvim_command('hi NonText guifg=bg')
 	vim.wo.winfixwidth = true
 	vim.wo.spell = false
 	-- Additional options to try out
@@ -151,45 +193,11 @@ local function _make_window_name(tabpage)
 	return 'SideKick(' .. tabpage .. ')'
 end
 
-
--- Make new window and move into it.
--- @param do_kick
-local function open_outline_window(do_kick, matches, highlight_info)
-	--TODO(ElPiloto): Make sure we have an open file otherwise this command will fail.
-	local tabpage = vim.api.nvim_get_current_tabpage()
-	local win_name = _make_window_name(tabpage)
-	local win, buf = nil, nil
-	if not open_windows[win_name] or vim.fn.win_id2win(open_windows[win_name]) == 0 then
-		win, buf = make_outline_window(win_name)
-		open_windows[win_name] = win
-	else
-		win = open_windows[win_name]
-		api.nvim_set_current_win(win)
-	end
-	
-	-- Display some ascii art text
-	local header, header_and_dude, header_and_poof = getSidekickText()
-	vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-	vim.api.nvim_buf_set_lines(buf, 0, #header_and_dude, false, header_and_dude)
-
-	-- Apparently this needs to be set after we insert text.
-	vim.api.nvim_buf_set_option(buf, 'modifiable', false)
-
-	if do_kick then
-		animateBufferOpen(win, header, matches, header_and_poof)
-	else
-		bufferOpen(win, header, matches)
-	end
-	sk_outline.set_highlight(highlight_info)
-	set_icon_highlights()
-	-- Also set sidekick_def_type_icons and outline as highlights
-	--sidekick_outer_node_icon
-	return buf, win
-end
-
-function set_icon_highlights()
+local function set_icon_highlights()
 	if vim.g.sidekick_outer_node_icon then
-		local syntax_group = "syntax match OuterInnerNodes '[" ..  table.concat({vim.g.sidekick_outer_node_icon, vim.g.sidekick_inner_node_icon}, '') .. "]'"
+		local syntax_group = "syntax match OuterInnerNodes '["
+		syntax_group = syntax_group .. table.concat({vim.g.sidekick_outer_node_icon, vim.g.sidekick_inner_node_icon}, '')
+		syntax_group = syntax_group .. "]'"
 		vim.cmd(syntax_group)
 		local highlight_link = "highlight default link OuterInnerNodes GruvBoxBlue"
 		vim.cmd(highlight_link)
@@ -211,6 +219,55 @@ function set_icon_highlights()
 	-- TODO (elpiloto): Make "String" configurable.
 end
 
+-- Searches current tabpage for a window containing buf.
+local function find_win_for_buf(buf)
+	local wins = api.nvim_tabpage_list_wins(0)
+	for _, win in ipairs(wins) do
+		if api.nvim_win_get_buf(win) == buf then
+			return win
+		end
+	end
+	return nil
+end
+
+-- Make new window and move into it.
+-- @param do_kick
+local function open_outline_window(do_kick, matches, highlight_info)
+	--TODO(ElPiloto): Make sure we have an open file otherwise this command will fail.
+	local tabpage = api.nvim_get_current_tabpage()
+	local win_name = _make_window_name(tabpage)
+	local win, buf = nil, nil
+	if not M.open_windows[win_name] or vim.fn.win_id2win(M.open_windows[win_name]) == 0 then
+		win, buf = make_outline_window(win_name)
+		M.open_windows[win_name] = win
+	else
+		win = M.open_windows[win_name]
+		api.nvim_set_current_win(win)
+		buf = api.nvim_get_current_buf()
+	end
+
+	-- Display some ascii art text
+	local header, header_and_dude, header_and_poof = getSidekickText()
+	api.nvim_buf_set_option(buf, 'modifiable', true)
+	api.nvim_buf_set_lines(buf, 0, #header_and_dude, false, header_and_dude)
+
+	-- Apparently this needs to be set after we insert text.
+	api.nvim_buf_set_option(buf, 'modifiable', false)
+
+	if do_kick then
+		animate_buffer_open(win, buf, header, matches, header_and_poof)
+	else
+		bufferOpen(win, buf, header, matches)
+	end
+	sk_outline.set_highlight(highlight_info)
+	set_icon_highlights()
+	add_keymappings()
+	-- Also set sidekick_def_type_icons and outline as highlights
+	--sidekick_outer_node_icon
+	return buf, win
+end
+
+
 local function entry_prefix(indent_level, is_last_node)
 	local prefix = '  '
 	if is_last_node then
@@ -224,7 +281,7 @@ end
 local function format_entry(def_name, def_type, indent_level, next_indent_level)
 	local is_last_node = false
 	local next_node_higher = false
-	if not next_indent_level then 
+	if not next_indent_level then
 		is_last_node = true
 	elseif indent_level ~= next_indent_level then
 		is_last_node = true
@@ -232,6 +289,7 @@ local function format_entry(def_name, def_type, indent_level, next_indent_level)
 			next_node_higher = true
 		end
 	end
+	next_node_higher = next_node_higher and nil -- Fix un-accessed variable error.
 	local str = entry_prefix(indent_level, is_last_node)
 	if vim.g.sidekick_def_type_icons[def_type] then
 		str = str .. vim.g.sidekick_left_bracket .. vim.g.sidekick_def_type_icons[def_type] .. vim.g.sidekick_right_bracket
@@ -240,11 +298,12 @@ local function format_entry(def_name, def_type, indent_level, next_indent_level)
 	return str
 end
 
-function foldtext()
+function M.foldtext()
 	local foldstart = vim.v.foldstart
-    local foldend   = vim.v.foldend
-    local winwidth  = vim.api.nvim_win_get_width(0)
-    local line, _ = (vim.api.nvim_buf_get_lines(0, foldstart - 1, foldstart, false)[1]):gsub('^"', ' ')
+    -- local foldend   = vim.v.foldend
+	-- Leaving this here just in case it's useful.
+    -- local winwidth  = api.nvim_win_get_width(0)
+    local line, _ = (api.nvim_buf_get_lines(0, foldstart - 1, foldstart, false)[1]):gsub('^"', ' ')
 	return line
 end
 
@@ -258,18 +317,18 @@ local function get_indent_level(line)
 end
 
 local function get_fold_context(line_num)
-	local before, current, after = nil, nil, nil
+	local before = nil
 	if line_num == 0 then
 		before = nil
 	else
-		before = (vim.api.nvim_buf_get_lines(0, line_num-1, line_num, false)[1])
+		before = (api.nvim_buf_get_lines(0, line_num-1, line_num, false)[1])
 	end
-	local lines, _ = vim.api.nvim_buf_get_lines(0, line_num, line_num+2, false)
+	local lines, _ = api.nvim_buf_get_lines(0, line_num, line_num+2, false)
 	return before, lines[1], lines[2]
 end
 
-function foldexpr()
-    local winwidth  = vim.api.nvim_win_get_width(0)
+function M.foldexpr()
+    --local winwidth  = api.nvim_win_get_width(0)
 	local line_num = vim.v.lnum - 1
 
 	-- Set no fold for anything in the header.
@@ -280,7 +339,6 @@ function foldexpr()
 	local before_indent = get_indent_level(before)
 	local line_indent = get_indent_level(line)
 	local after_indent = get_indent_level(after)
-	--local line, _ = (vim.api.nvim_buf_get_lines(0, line_num, line_num+1, false)[1]):gsub('^"', ' ')
 	-- If we're a blank line or surrounded by blank lines, we are not a fold.
 	if line == '' or (before == '' and after == '') then
 		return "0"
@@ -300,12 +358,18 @@ end
 
 local function get_outline()
 	-- TODO(ElPiloto): check filetype is parsable, otherwise return nil
-	local bufnr = vim.api.nvim_get_current_buf()
+	local bufnr = api.nvim_get_current_buf()
 	-- TODO(ElPiloto): makes 'locals' configurable.
-	scopes, defs, scopes_and_defs = sk_outline.get_scope_and_definition_captures(bufnr, 'locals')
-	root = sk_outline.find_parents(scopes_and_defs, scopes)
-	outline, hl_info = sk_outline.build_outline(root, query)
+	local scopes, defs, scopes_and_defs = sk_outline.get_scope_and_definition_captures(bufnr, 'locals')
+	defs = defs and nil -- Fix lint error
+	local root = sk_outline.find_parents(scopes_and_defs, scopes)
+	-- NB
+	--table.insert(ranges, {def_name, def_type, indent, start_row, start_col, end_row, end_col})
+	local outline, hl_info = sk_outline.build_outline(root)
+	-- Note: These are relative line numbers from the start of our outline, not the window.
+	local jump_info = {}
 	local indented_strings = {}
+	local line_nr = 1
 	for i, info in ipairs(outline) do
 		local def_type = info[2]
 		local next_indent_level = nil
@@ -321,17 +385,21 @@ local function get_outline()
 				end
 			end
 			table.insert(indented_strings, format_entry(info[1], info[2], info[3], next_indent_level))
+			jump_info[line_nr] = {info[4], info[5]}
+			line_nr = line_nr + 1
 			if  next_indent_level and next_indent_level < info[3] then
 				table.insert(indented_strings, '')
+				line_nr = line_nr + 1
 			end
 			if  next_indent_level and next_indent_level == 0 and info[3] == 0 then
 				table.insert(indented_strings, '')
+				line_nr = line_nr + 1
 			end
 		end
 	end
 	-- Add a final blank line.
 	table.insert(indented_strings, '')
-	return indented_strings, hl_info
+	return indented_strings, hl_info, jump_info
 end
 
 
@@ -343,24 +411,44 @@ local function enable_folding()
 end
 
 
-local function run()
-	local buf = vim.api.nvim_get_current_buf()
-	if not sk_outline.can_parse_buffer(bufnr) then
+function M.jump_to_definition()
+	-- TODO(elpiloto): Make this more robust when we switch to the render interface.
+	local start_outline_line_nr = #split_str(M.header)
+	local abs_line_nr = vim.fn.line('.')
+	local relative_line_nr = abs_line_nr - start_outline_line_nr
+	-- TODO(elpiloto): Reload cursor location (a.k.a. nodes from Treesitter) if
+	-- needed.  This may be the case if the buffer has been modified since we
+	-- last populated per_buffer_jump_info.
+	if M.per_buffer_jump_info[M.last_parsed_buf] then
+		if M.per_buffer_jump_info[M.last_parsed_buf][relative_line_nr] then
+			local row_col =  M.per_buffer_jump_info[M.last_parsed_buf][relative_line_nr]
+			local win = find_win_for_buf(M.last_parsed_buf)
+			if win then
+				print('we in', vim.inspect(row_col), M.last_parsed_buf)
+				--api.nvim_set_current_buf(M.last_parsed_buf)
+				api.nvim_set_current_win(win)
+				api.nvim_win_set_cursor(0, { row_col[1] + 1, row_col[2] })
+			end
+		end
+	end
+end
+
+
+function M.run()
+	local buf = api.nvim_get_current_buf()
+	if not sk_outline.can_parse_buffer(buf) then
 		local filetype = vim.bo.filetype
 		print('No parser for filetype: ' .. filetype)
 		return
 	end
-	local processed_matches, hl_info = get_outline()
+	M.last_parsed_buf = buf
+	print('Setting last parsed buf to ' .. tostring(M.last_parsed_buf))
+	local processed_matches, hl_info, jump_info = get_outline()
+	M.per_buffer_jump_info[M.last_parsed_buf] = jump_info
 	local do_kick = false
-	buf, win = open_outline_window(do_kick, processed_matches, hl_info)
+	open_outline_window(do_kick, processed_matches, hl_info)
 	enable_folding()
 end
 
 
-return {
-	run = run,
-	foldtext = foldtext,
-	foldexpr = foldexpr,
-	loaded_buffers = loaded_buffers,
-	open_windows = open_windows,
-}
+return M
